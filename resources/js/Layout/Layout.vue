7c32aef3d42fae12bbb4249193c24511257f5e12
Mark Ciril <markciril.jamon@gmail.com>
2025-07-13 16:17:50 +0800

    feat: implement routing to different component page

---
+++ resources/js/Layout/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="h-24 bg-blue-200">
+        <div class="flex gap-4 items-center text-center">
+            <Link href="/">Home</Link>
+            <Link href="/">About
+            </Link>
+        </div>
+    </div>
+    <main>
+        <slot></slot>
+    </main>
+</template>
+
+<script setup>
+import { Link, Head } from '@inertiajs/vue3';
+</script>
