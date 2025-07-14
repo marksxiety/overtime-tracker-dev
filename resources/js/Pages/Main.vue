518c6158c14379cc30f3befcd19ebe927f48b389
Mark Ciril <markciril.jamon@gmail.com>
2025-07-14 17:19:13 +0800

    style: create a card that will display the total OT hours and pending requests

--- resources/js/Pages/Main.vue
+++ resources/js/Pages/Main.vue
@@ -1,5 +1,14 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="flex flex-col">
+        <div class="grid grid-cols-2 gap-4">
+            <div class="col-span-1 flex flex-col border-2 rounded p-4 bg-gray-200">
+                <span class="text-md font-semibold">Total Overtime Hours</span>
+                <span class="text-sm font-extrabold">45</span>
+            </div>
+            <div class="col-span-1 flex flex-col border-2 rounded p-4 bg-gray-200">
+                <span class="text-md font-semibold">Pending Requests</span>
+                <span class="text-sm font-extrabold">2</span>
+            </div>
+        </div>
+    </div>
 </template>
