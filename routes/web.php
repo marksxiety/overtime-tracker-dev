455f5e9bc2367dbd9065e1fbacd3f97eee055ce0
Mark Ciril <markciril.jamon@gmail.com>
2025-07-14 16:55:13 +0800

    chore: route for login that access the login function in AuthController

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,6 @@ Route::inertia('/', 'Main')->name('main');
 Route::middleware(['guest'])->group(function () {
     Route::inertia('/register', 'Auth/Register')->name('register');
     Route::post('/register', [AuthController::class, 'register']);
-
+    Route::post('/login', [AuthController::class, 'login']);
     Route::inertia('/login', 'Auth/Login')->name('login');
 });
