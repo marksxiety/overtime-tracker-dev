749c3e47dfac5ea5ec029b67efd24f45bc97a651
Mark Ciril <markciril.jamon@gmail.com>
2025-07-14 17:18:52 +0800

    style: implement proper header (layout) for overall project

--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="h-24 bg-blue-200">
-        <div class="flex gap-4 items-center text-center">
-            <Link :href="route('main')">Home</Link>
-        </div>
+    <div class="flex justify-between bottom-2 shadow-md h-full p-4">
+         <Link :href="route('main')" class="font-bold text-sm">Overtime Tracker</Link>
+         <div class="flex justify-between gap-4 font-semibold">
+            <p class="text-sm hover:underline hover:cursor-pointer">Dashboard</p>
+            <p class="text-sm hover:underline hover:cursor-pointer">Requests</p>
+            <p class="text-sm hover:underline hover:cursor-pointer">Reports</p>
+            <p class="text-sm hover:underline hover:cursor-pointer">Profile</p>
+         </div>
     </div>
-    <main>
+    <main class="m-4 pr-20 pl-20 pt-4 pb-4 f-full max-h-screen">
         <slot></slot>
     </main>
 </template>
