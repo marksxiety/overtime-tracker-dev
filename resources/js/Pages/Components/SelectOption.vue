7922cf7ce52bbf6da004fde6f56a2ede37e9084c
Mark Ciril <markciril.jamon@gmail.com>
2025-07-15 11:27:25 +0800

    style: implement new class for select option

--- resources/js/Pages/Components/SelectOption.vue
+++ resources/js/Pages/Components/SelectOption.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="mb-4">
-        <label for="role" class="block text-gray-700 mb-2">{{ name }}</label>
-        <select id="role" v-model="model" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2">
-            <option selected value="" class="text-gray-700">Choose a role</option>
-            <option value="employee" class="text-gray-700">Employee</option>
-            <option value="approver" class="text-gray-700">Approver</option>
-            <option value="admin" class="text-gray-700">Admin</option>
+        <label for="role" class="block mb-2">{{ name }}</label>
+        <select id="role" v-model="model" class="w-full px-4 py-2 select">
+            <option selected value="">Choose a role</option>
+            <option value="employee">Employee</option>
+            <option value="approver">Approver</option>
+            <option value="admin">Admin</option>
         </select>
         <p v-if="message" class="mt-1 text-sm text-red-600 bg-red-50 px-2 py-1 rounded text-center">
             {{ message }}
