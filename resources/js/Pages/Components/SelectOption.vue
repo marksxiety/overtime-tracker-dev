9b27761b72ae6e5ba333f093fdb5097b5f499ef8
Mark Ciril <markciril.jamon@gmail.com>
2025-07-16 19:14:35 +0800

    refactor: update the option population from already defined to prop-based in able to have a dynamic value

--- resources/js/Pages/Components/SelectOption.vue
+++ resources/js/Pages/Components/SelectOption.vue
@@ -2,10 +2,9 @@
     <div class="mb-4">
         <label for="role" class="block mb-2">{{ name }}</label>
         <select id="role" v-model="model" class="w-full px-4 py-2 select">
-            <option selected value="">Choose a role</option>
-            <option value="employee">Employee</option>
-            <option value="approver">Approver</option>
-            <option value="admin">Admin</option>
+            <option v-for="option in options" :key="option.value" :value="option.value">
+                {{ option.label }}
+            </option>
         </select>
         <p v-if="message" class="mt-1 text-sm text-red-600 bg-red-50 px-2 py-1 rounded text-center">
             {{ message }}
@@ -19,6 +18,7 @@ defineProps({
         type: String
     },
     message: String,
+    options: Array
 })
 
 const model = defineModel({
