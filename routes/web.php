8b147d5719312008eca5b46e6d463c2d8eba2ae2
Mark Ciril <markciril.jamon@gmail.com>
2025-07-16 15:00:09 +0800

    chore: update the guest and auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
-
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::inertia('/', 'Main')->name('main');
 
 Route::middleware(['guest'])->group(function () {
     Route::inertia('/register', 'Auth/Register')->name('register');
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::inertia('/login', 'Auth/Login')->name('login');
+});
 
-    Route::inertia('/request', 'Employee.Request')->name('request');
+Route::middleware(['auth'])->group(function () {
+    Route::inertia('/request', 'Employee/Request')->name('request');
+    Route::inertia('/', 'Main')->name('main');
 });
