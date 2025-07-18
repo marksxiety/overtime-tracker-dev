3f12e9ba94d387b78f0a08594d006805f0004dc5
Mark Ciril <markciril.jamon@gmail.com>
2025-07-19 01:14:22 +0800

    chore: add delete route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/shift', [ShiftContoller::class, 'registeredShiftCodes'])->name('shifts');
     Route::post('/shift/register', [ShiftContoller::class, 'insertShiftCode'])->name('shift.register'); // insertion route
     Route::put('/shift/{shift}', [ShiftContoller::class, 'updateShiftCode'])->name('shift.update'); // update route
+    Route::delete('/shift/{shift}', [ShiftContoller::class, 'deleteShiftCode'])->name('shift.delete'); // delete route
 });
