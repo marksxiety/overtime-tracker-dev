96483d16e2b219595561088126ffb0176f5eb8b0
Mark Ciril <markciril.jamon@gmail.com>
2025-07-19 02:34:27 +0800

    feat: create a modal component with dynamic content

---
+++ resources/js/Pages/Components/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog class="modal" ref="dialog">
+        <div class="modal-box">
+            <slot></slot>
+        </div>
+    </dialog>
+</template>
+
+<script setup>
+import { ref, defineExpose } from 'vue'
+
+const dialog = ref(null)
+
+const open = () => dialog.value?.showModal()
+const close = () => dialog.value?.close()
+
+defineExpose({ open, close })
+</script>
