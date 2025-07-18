26b94974ccbe272ef8f48a7d6eb76033211f8751
Mark Ciril <markciril.jamon@gmail.com>
2025-07-19 01:23:45 +0800

    style: update the web icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
+  <link rel="icon" href="{{ asset('assets/dev.png') }}" type="image/png">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   @vite('resources/js/app.js')
   @inertiaHead
