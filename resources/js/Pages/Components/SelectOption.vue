20d446c34dfacfe5c993a2ff0e53e8e3769d3b23
Mark Ciril <markciril.jamon@gmail.com>
2025-07-19 16:19:38 +0800

    style: add border color if message is available for better ux

--- resources/js/Pages/Components/SelectOption.vue
+++ resources/js/Pages/Components/SelectOption.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="mb-4">
         <label for="role" class="block mb-2">{{ name }}</label>
-        <select id="role" v-model="model" class="w-full px-4 py-2 select">
+        <select id="role" v-model="model"
+            :class="['w-full px-4 py-2 input', message ? 'border-red-500 focus:ring-red-200' : 'border-gray-300 focus:ring-blue-200', 'w-full px-4 py-2 select']">
             <option v-for="option in options" :key="option.value" :value="option.value" :selected="option.selected">
                 {{ option.label }}
             </option>
         </select>
-        <p v-if="message" class="mt-1 text-sm text-red-600 bg-red-50 px-2 py-1 rounded text-center">
+        <p v-if="message" class="mt-1 text-sm text-red-600 px-2 py-1 text-center">
             {{ message }}
         </p>
     </div>
