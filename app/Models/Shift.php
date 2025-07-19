508feef63036886877d4969f560e9183d23bff6b
Mark Ciril <markciril.jamon@gmail.com>
2025-07-19 12:32:51 +0800

    chore: add propery to remove the warning in IDE when using $id in the Shift model

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * App\Models\Shift
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $start_time
+ * @property string $end_time
+ */
 class Shift extends Model
 {
     use HasFactory;
