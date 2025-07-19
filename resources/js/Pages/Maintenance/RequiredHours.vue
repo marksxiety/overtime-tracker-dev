9496b3b6d915957a213ff3ede3fadf5d5390fe90
Mark Ciril <markciril.jamon@gmail.com>
2025-07-19 15:53:40 +0800

    style: update the layout for better grid viewing

--- resources/js/Pages/Maintenance/RequiredHours.vue
+++ resources/js/Pages/Maintenance/RequiredHours.vue
@@ -1,9 +1,23 @@
 <template>
     <div class="flex flex-col gap-6 h-full">
-        <h3 class="font-semibold">Required Hours Registration</h3>
-        <div class="grid grid-cols-2 gap-6 h-96">
+        <div class="breadcrumbs text-sm">
+            <ul>
+                <li>
+                    <Link :href="route('main')">Home</Link>
+                </li>
+                <li>
+                    <Link :href="route('hours')">Required Hours Registration</Link>
+                </li>
+            </ul>
+        </div>
+        <!-- Page Heading -->
+        <div class="flex justify-between items-center">
+            <h1 class="text-2xl font-bold">Manage Required Hours</h1>
+        </div>
+
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 h-[32rem]">
             <div class="col-span-1">
-                <div class="max-w-md mx-auto bg-base-300 p-8 rounded-xl shadow h-full">
+                <div class="bg-base-300 p-6 rounded-2xl shadow-xl h-full flex flex-col justify-center">
                     <form @submit.prevent="submitForm()" class="card">
                         <SelectOption name="Year:" :options="years" v-model="form.year" :message="form.errors.year" />
                         <SelectOption name="Week:" :options="weeks" v-model="form.week" :message="form.errors.week" />
@@ -16,20 +30,35 @@
                 </div>
             </div>
             <div class="col-span-1">
-                <div class="bg-base-300 rounded-box px-4 h-96 overflow-auto">
-                    <table class="table">
+                <div class="bg-base-300 rounded-2xl p-4 h-full overflow-auto shadow-xl">
+                    <h2 class="text-lg font-semibold mb-4">Registered Required Hours</h2>
+                    <table class="table table-zebra w-full">
                         <thead class="sticky top-0 bg-base-300 z-10">
-                            <tr>
+                            <tr class="text-center">
                                 <th>Year</th>
                                 <th>Week</th>
                                 <th>Required Hours</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="required in registerd_required_hours" :key="required.required_code">
+                            <tr v-for="required in registerd_required_hours" :key="required.required_code"
+                                class="text-center">
                                 <td>{{ required.year }}</td>
                                 <td>{{ required.week }}</td>
                                 <td>{{ `${required.required_hours} hrs` }}</td>
+                                <td class="flex flex-row gap-2 justify-center">
+                                    <button class="btn btn-success btn-xs">
+                                        EDIT
+                                    </button>
+                                    <button class="btn btn-error btn-xs">
+                                        <span>DELETE</span></button>
+                                </td>
+                            </tr>
+                            <tr v-if="registerd_required_hours.length === 0">
+                                <td colspan="4" class="text-center italic text-gray-400 py-4">
+                                    No required hours available.
+                                </td>
                             </tr>
                         </tbody>
                     </table>
@@ -43,7 +72,7 @@
 import TextInput from '../Components/TextInput.vue'
 import SelectOption from '../Components/SelectOption.vue'
 import { ref, inject, watch } from 'vue'
-import { useForm, usePage } from '@inertiajs/vue3'
+import { useForm, usePage, Link } from '@inertiajs/vue3'
 
 const toast = inject('toast')
 const page = usePage()
