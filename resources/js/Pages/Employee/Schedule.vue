551d5595423589443edfcade1ddd12b97591cdf3
Mark Ciril <markciril.jamon@gmail.com>
2025-07-20 19:00:18 +0800

    style: initial design for managing schedules

---
+++ resources/js/Pages/Employee/Schedule.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-col gap-6 h-full">
+        <div class="breadcrumbs text-sm">
+            <ul>
+                <li>
+                    <Link :href="route('main')">Home</Link>
+                </li>
+                <li>
+                    <Link :href="route('schedule')">Manage Schedule</Link>
+                </li>
+            </ul>
+        </div>
+        <!-- Page Heading -->
+        <div class="flex justify-between items-center">
+            <h1 class="text-2xl font-bold">Manage Schedule</h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
