b246153df3bc64a7d9e21bf4cf9da6e0c9c5c1a4
Mark Ciril <markciril.jamon@gmail.com>
2025-07-21 18:23:48 +0800

    feat: add year and week selection options to schedule management

--- resources/js/Pages/Employee/Schedule.vue
+++ resources/js/Pages/Employee/Schedule.vue
@@ -13,10 +13,38 @@
         <!-- Page Heading -->
         <div class="flex justify-between items-center">
             <h1 class="text-2xl font-bold">Manage Schedule</h1>
+            <div class="grid grid-cols-2 gap-4 w-1/3">
+                <div class="col-span-1 w-full">
+                <SelectOption name="Year" :options="years"/>
+                </div>
+                <div class="col-span-1 w-full">
+                <SelectOption name="Week" :options="weeks"/>
+                </div>
+            </div>
         </div>
+        <hr>
     </div>
 </template>
 
 <script setup>
+import SelectOption from '../Components/SelectOption.vue'
 
+const weeks = Array.from({ length: 52 }, (_, i) => {
+    const weekNum = String(i + 1).padStart(2, '0')
+    return {
+        label: `Week: ${weekNum}`,
+        value: `${parseInt(weekNum)}`
+    }
+})
+
+const years = [
+    { label: "Select Year", value: "", selected: true },
+    { label: "2024", value: "2024" },
+    { label: "2025", value: "2025" },
+    { label: "2026", value: "2026" },
+    { label: "2027", value: "2027" },
+    { label: "2028", value: "2028" },
+    { label: "2029", value: "2029" },
+    { label: "2030", value: "2030" }
+]
 </script>
