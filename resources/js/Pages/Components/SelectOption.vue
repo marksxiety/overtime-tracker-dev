f879917a37e883e66dfaaa927944c682bf513c20
Mark Ciril <markciril.jamon@gmail.com>
2025-07-24 17:40:36 +0800

    chore: add conditional displaying of label and style of option values

--- resources/js/Pages/Components/SelectOption.vue
+++ resources/js/Pages/Components/SelectOption.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="mb-4">
-        <label for="role" class="block mb-2">{{ name }}</label>
+        <label v-if="name" for="role" class="block mb-2">{{ name }}</label>
         <select id="role" v-model="model"
             :class="['w-full px-4 py-2 input', message ? 'border-red-500 focus:ring-red-200' : 'border-gray-300 focus:ring-blue-200', 'w-full px-4 py-2 select']">
-            <option v-for="option in options" :key="option.value" :value="option.value">
+            <option v-for="option in options" :key="option.value" :value="option.value" class="text-center">
                 {{ option.label }}
             </option>
         </select>
