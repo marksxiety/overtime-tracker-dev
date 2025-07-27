480ce58f67ede7ee79bfd838a8b16c97212b84b6
Mark Ciril <markciril.jamon@gmail.com>
2025-07-27 17:55:47 +0800

    style: initial content of request overtime page

--- resources/js/Pages/Employee/Request.vue
+++ resources/js/Pages/Employee/Request.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>FIle Here</h1>
+    <div class="flex flex-col gap-6 h-full">
+        <div class="breadcrumbs text-sm">
+            <ul>
+                <li>
+                    <Link :href="route('main')">Home</Link>
+                </li>
+                <li>
+                    <Link :href="route('request')">Overtime Request</Link>
+                </li>
+            </ul>
+        </div>
+        <div class="flex justify-between items-center">
+            <h1 class="text-2xl font-bold">Overtime Request</h1>
+        </div>
+        <hr>
+    </div>
 </template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
