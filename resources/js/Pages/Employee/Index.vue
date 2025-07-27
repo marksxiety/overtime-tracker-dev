e2b98f423982b4a1938e0299ae28d21116d3b566
Mark Ciril <markciril.jamon@gmail.com>
2025-07-27 17:56:04 +0800

    style: implement proper displaying of date using sample data

--- resources/js/Pages/Employee/Index.vue
+++ resources/js/Pages/Employee/Index.vue
@@ -18,6 +18,69 @@
             Manage Schedules
             </Link>
         </div>
+
+        <div class="h-screen mt-4">
+            <div class="flex flex-col gap-4 border rounded-md p-4">
+                <!-- Header -->
+                <header class="flex items-center justify-between">
+                    <button class="btn btn-sm btn-neutral">&lt;</button>
+                    <p class="current-date font-semibold text-md">September 2025</p>
+                    <button class="btn btn-sm btn-neutral">&gt;</button>
+                </header>
+
+                <hr>
+                <div class="calendar">
+                    <ul class="grid grid-cols-7 gap-6 text-center font-semibold text-md">
+                        <li>Sun</li>
+                        <li>Mon</li>
+                        <li>Tue</li>
+                        <li>Wed</li>
+                        <li>Thu</li>
+                        <li>Fri</li>
+                        <li>Sat</li>
+                    </ul>
+
+                    <ul class="grid grid-cols-7 gap-4 text-center mt-2 text-md">
+                        <li class="p-2 rounded text-gray-400 hover:bg-gray-100">28</li>
+                        <li class="p-2 rounded text-gray-400 hover:bg-gray-100">29</li>
+                        <li class="p-2 rounded text-gray-400 hover:bg-gray-100">30</li>
+                        <li class="p-2 rounded hover:bg-gray-100">1</li>
+                        <li class="p-2 rounded hover:bg-gray-100">2</li>
+                        <li class="p-2 rounded hover:bg-gray-100">3</li>
+                        <li class="p-2 rounded hover:bg-gray-100">4</li>
+                        <li class="p-2 rounded hover:bg-gray-100">5</li>
+                        <li class="p-2 rounded hover:bg-gray-100">6</li>
+                        <li class="p-2 rounded hover:bg-gray-100">7</li>
+                        <li class="p-2 rounded hover:bg-gray-100">8</li>
+                        <li class="p-2 rounded hover:bg-gray-100">9</li>
+                        <li class="p-2 rounded hover:bg-gray-100">10</li>
+                        <li class="p-2 rounded hover:bg-gray-100">11</li>
+                        <li class="p-2 rounded hover:bg-gray-100">12</li>
+                        <li class="p-2 rounded hover:bg-gray-100">13</li>
+                        <li class="p-2 rounded hover:bg-gray-100">14</li>
+                        <li class="p-2 rounded hover:bg-gray-100">15</li>
+                        <li class="p-2 rounded hover:bg-gray-100">16</li>
+                        <li class="p-2 rounded hover:bg-gray-100">17</li>
+                        <li class="p-2 rounded hover:bg-gray-100">18</li>
+                        <li class="p-2 rounded hover:bg-gray-100">19</li>
+                        <li class="p-2 rounded hover:bg-gray-100">20</li>
+                        <li class="p-2 rounded hover:bg-gray-100">21</li>
+                        <li class="p-2 rounded hover:bg-gray-100">22</li>
+                        <li class="p-2 rounded hover:bg-gray-100">23</li>
+                        <li class="p-2 rounded hover:bg-gray-100">24</li>
+                        <li class="p-2 rounded hover:bg-gray-100">25</li>
+                        <li class="p-2 rounded hover:bg-gray-100">26</li>
+                        <li class="p-2 rounded hover:bg-gray-100">27</li>
+                        <li class="p-2 rounded hover:bg-gray-100">28</li>
+                        <li class="p-2 rounded hover:bg-gray-100">29</li>
+                        <li class="p-2 rounded hover:bg-gray-100">30</li>
+                        <li class="p-2 rounded text-gray-400 hover:bg-gray-100">1</li>
+                        <li class="p-2 rounded text-gray-400 hover:bg-gray-100">2</li>
+                        <li class="p-2 rounded text-gray-400 hover:bg-gray-100">3</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
