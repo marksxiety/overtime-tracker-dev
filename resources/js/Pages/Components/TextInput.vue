e439c5c5860c1bd76c6dc875353e92f57a79b07f
Mark Ciril <markciril.jamon@gmail.com>
2025-07-30 09:57:13 +0800

    chore: add extra property of readonly and disabled with false default value

--- resources/js/Pages/Components/TextInput.vue
+++ resources/js/Pages/Components/TextInput.vue
@@ -3,7 +3,7 @@
         <label class="block mb-2" for="name">{{ name }}</label>
         <input :id="name" :type="type" v-model="model"
             :class="['w-full px-4 py-2 input', message ? 'border-red-500 focus:ring-red-200' : 'border-gray-300 focus:ring-blue-200', textCase]"
-            :placeholder="placeholder" />
+            :placeholder="placeholder" :disabled="disabled" :readonly="readonly"/>
         <p v-if="message" class="mt-1 text-sm text-red-600 px-2 py-1 text-center">
             {{ message }}
         </p>
@@ -12,6 +12,8 @@
 
 
 <script setup>
+import { readonly } from 'vue';
+
 defineProps({
     name: {
         type: String
@@ -28,6 +30,14 @@ defineProps({
     textCase: {
         type: String,
         default: ''
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    },
+    readonly: {
+        type: Boolean,
+        default: false
     }
 })
 
