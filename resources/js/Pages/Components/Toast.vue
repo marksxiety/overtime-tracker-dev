4903ca62a51cbbe6bea3e29af3fe25834bc04f4d
Mark Ciril <markciril.jamon@gmail.com>
2025-07-31 16:23:35 +0800

    style: adjust the location of the toast

--- resources/js/Pages/Components/Toast.vue
+++ resources/js/Pages/Components/Toast.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="toast toast-top toast-end fixed z-50">
-        <div v-for="(toast, i) in toasts" :key="i" class="alert" :class="{
-            'alert-success': toast.type === 'success',
-            'alert-error': toast.type === 'error',
-            'alert-info': toast.type === 'info',
-            'alert-warning': toast.type === 'warning'
-        }">
-            {{ toast.message }}
-        </div>
+  <div class="toast toast-top toast-end fixed z-50 mt-6 mr-4">
+    <div
+      v-for="(toast, i) in toasts"
+      :key="i"
+      class="alert border-2 rounded"
+      :class="[
+        toast.type === 'success' ? 'alert-success border-success' :
+        toast.type === 'error' ? 'alert-error border-error' :
+        toast.type === 'info' ? 'alert-info border-info' :
+        toast.type === 'warning' ? 'alert-warning border-warning' :
+        ''
+      ]"
+    >
+      {{ toast.message }}
     </div>
+  </div>
 </template>
+
 
 <script setup>
 import { ref } from 'vue'
@@ -20,7 +27,7 @@ function showToast(message, type = 'info') {
     toasts.value.push({ message, type })
     setTimeout(() => {
         toasts.value.shift()
-    }, 3000)
+    }, 5000)
 }
 
 defineExpose({ showToast })
