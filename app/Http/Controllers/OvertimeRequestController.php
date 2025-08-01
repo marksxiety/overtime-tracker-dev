41385f2a8ae0c06c250873aada3481111e0426ea
Mark Ciril <markciril.jamon@gmail.com>
2025-08-01 13:48:43 +0800

    feat: handle at the end the validator result

--- app/Http/Controllers/OvertimeRequestController.php
+++ app/Http/Controllers/OvertimeRequestController.php
@@ -6,15 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\OvertimeRequest;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 class OvertimeRequestController extends Controller
 {
     public function insertOvertimeRequest(Request $request)
     {
 
-        // dd($request);
-
-        $data = $request->validate([
+        $validator = Validator::make($request->all(), [
             'employee_schedule_id' => 'exists:schedules,id|required',
             'date' => 'required|date_format:Y-m-d',
             'start_time' => 'required|date_format:H:i',
@@ -34,19 +33,27 @@ class OvertimeRequestController extends Controller
         $start_diff = $schdule_start_time->diffInMinutes($submitted_start_time, false); // negative if earlier
         $end_diff = $schdule_end_time->diffInMinutes($submitted_end_time, false);       // positive if later
 
-        $errors = [];
+        $errors = $validator->errors();
 
         if ($start_diff > -60) {
-            $errors['start_time'] = 'Start time should be at least 1 hour before the scheduled start time.';
+            $errors->add('start_time','Start time should be at least 1 hour before the scheduled start time.');
         }
 
         if ($end_diff < 60) {
-            $errors['end_time'] = 'End time should be at least 1 hour after the scheduled end time.';
+            $errors->add('end_time','End time should be at least 1 hour after the scheduled end time.');
         }
 
-        if ($errors) {
+        if ($errors->any()) {
             return redirect()->back()->withErrors($errors)->withInput();
         }
+
+        $data = [
+            'employee_schedule_id' => $request->employee_schedule_id,
+            'date' => $request->date,
+            'start_time' => $request->start_time,
+            'end_time' => $request->end_time,
+            'reason' => $request->reason
+        ];
 
         OvertimeRequest::create($data);
         return redirect()->back()->with(['message' => 'Overtime Request has been filed!']);
