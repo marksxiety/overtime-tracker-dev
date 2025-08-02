c94dadb8672ea69cc01dff1fc465a87f82eac705
Mark Ciril <markciril.jamon@gmail.com>
2025-08-02 23:04:15 +0800

    refactor: use different type of role to have a specific route they can access

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ShiftContoller;
 use App\Http\Controllers\RequiredHoursController;
 use App\Http\Controllers\ScheduleController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::middleware(['guest'])->group(function () {
     Route::inertia('/register', 'Auth/Register')->name('register');
@@ -14,25 +15,12 @@ Route::middleware(['guest'])->group(function () {
     Route::inertia('/login', 'Auth/Login')->name('login');
 });
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware('employee')->group(function () {
     Route::inertia('/request', 'Employee/Request')->name('request');
     Route::inertia('/', 'Main')->name('main');
 
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-    // shift code routes
-    Route::get('/shift', [ShiftContoller::class, 'registeredShiftCodes'])->name('shifts');
-    Route::post('/shift/register', [ShiftContoller::class, 'insertShiftCode'])->name('shift.register'); // insertion route
-    Route::put('/shift/{shift}', [ShiftContoller::class, 'updateShiftCode'])->name('shift.update'); // update route
-    Route::delete('/shift/{shift}', [ShiftContoller::class, 'deleteShiftCode'])->name('shift.delete'); // delete route
-
-    // shift code requests (axios)
+    // shift code list for registering schedule requests (axios)
     Route::get('/shift/list', [ShiftContoller::class, 'shiftCodeList']);
-
-    // required hours routes
-    Route::get('/hours', [RequiredHoursController::class, 'registeredRequiredHours'])->name('hours');
-    Route::post('/hours/register', [RequiredHoursController::class, 'registerRequiredHours'])->name('hours.register');
-    Route::put('/hours/{requiredHours}', [RequiredHoursController::class, 'updateRequiredHour'])->name('hours.update');
 
     // schedule routes
     Route::inertia('/schedule', 'Employee/Schedule')->name('schedule');
@@ -44,3 +32,22 @@ Route::middleware(['auth'])->group(function () {
     Route::post('overtime/request', [OvertimeRequestController::class, 'insertOvertimeRequest'])->name('overtime.request');
     Route::get('/overtime/list', [ScheduleController::class, 'fetchOvertimeRequetsBySession']);
 });
+
+Route::middleware('admin')->group(function () {
+    // shift code requests (axios)
+    Route::get('/shift', [ShiftContoller::class, 'registeredShiftCodes'])->name('shifts');
+    Route::post('/shift/register', [ShiftContoller::class, 'insertShiftCode'])->name('shift.register'); // insertion route
+    Route::put('/shift/{shift}', [ShiftContoller::class, 'updateShiftCode'])->name('shift.update'); // update route
+    Route::delete('/shift/{shift}', [ShiftContoller::class, 'deleteShiftCode'])->name('shift.delete'); // delete route
+});
+
+Route::middleware('approver')->group(function () {
+    // required hours routes
+    Route::get('/hours', [RequiredHoursController::class, 'registeredRequiredHours'])->name('hours');
+    Route::post('/hours/register', [RequiredHoursController::class, 'registerRequiredHours'])->name('hours.register');
+    Route::put('/hours/{requiredHours}', [RequiredHoursController::class, 'updateRequiredHour'])->name('hours.update');
+});
+
+Route::get('/unauthorized', fn() => Inertia::render('Unauthorized'))->name('unauthorized');
+
+Route::middleware('auth')->post('/logout', [AuthController::class, 'logout'])->name('logout');
