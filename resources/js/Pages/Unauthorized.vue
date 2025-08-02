7014a599e3d66942f7c892be9851ce4f0cd7db95
Mark Ciril <markciril.jamon@gmail.com>
2025-08-02 23:02:20 +0800

    style: initial design for unathorized page

---
+++ resources/js/Pages/Unauthorized.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-4xl font-bold text-red-600 mb-4">403 - Unauthorized</h1>
+        <p class="text-gray-700 text-lg mb-6">
+            You do not have permission to access this page.
+        </p>
+        <Link href="/" class="text-blue-600 hover:underline">
+        ← Go back to Home
+        </Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
