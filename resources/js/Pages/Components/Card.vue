537e1a40880ba18dc21f80c658d99c69cc0af2bf
Mark Ciril <markciril.jamon@gmail.com>
2025-08-04 13:18:53 +0800

    feat: create a reusable component for card tile for dashboard

---
+++ resources/js/Pages/Components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card rounded bg-base-100 shadow-sm p-4 flex flex-row items-center justify-between">
+        <span class="text-sm font-semibold opacity-80">{{ label }}</span>
+        <span class="text-2xl font-extrabold">{{ value }} hrs</span>
+    </div>
+</template>
+
+<script setup>
+
+defineProps({
+    label: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
