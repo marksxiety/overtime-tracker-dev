c47161d45f09cd947b665edbb05be682400dfc65
Mark Ciril <markciril.jamon@gmail.com>
2025-08-04 09:33:09 +0800

    style: remove border for all elements and make ng bg-base stands out the element

--- resources/js/Pages/Employee/Index.vue
+++ resources/js/Pages/Employee/Index.vue
@@ -184,19 +184,19 @@
     <div class="flex flex-col gap-4">
         <div class="grid grid-cols-3 gap-4">
             <!-- Total Overtime Hours -->
-            <div class="card border rounded bg-base-100 shadow-sm p-4 flex flex-row items-center justify-between">
+            <div class="card rounded bg-base-100 shadow-sm p-4 flex flex-row items-center justify-between">
                 <span class="text-sm font-semibold opacity-80">Total Overtime Hours</span>
                 <span class="text-2xl font-extrabold">{{ totalovertime }} hrs</span>
             </div>
 
             <!-- Pending Requests -->
-            <div class="card border rounded bg-base-100 shadow-sm p-4 flex flex-row items-center justify-between">
+            <div class="card rounded bg-base-100 shadow-sm p-4 flex flex-row items-center justify-between">
                 <span class="text-sm font-semibold opacity-80">Pending Requests</span>
                 <span class="text-2xl font-extrabold">{{ pendingovertime }}</span>
             </div>
 
             <!-- Rejected Requests -->
-            <div class="card border rounded bg-base-100 shadow-sm p-4 flex flex-row items-center justify-between">
+            <div class="card rounded bg-base-100 shadow-sm p-4 flex flex-row items-center justify-between">
                 <span class="text-sm font-semibold opacity-80">Rejected Requests</span>
                 <span class="text-2xl font-extrabold">{{ rejectedovertime }}</span>
             </div>
@@ -209,7 +209,7 @@
         </div>
 
         <div class="flex justify-between gap-4">
-            <div class="border rounded-md p-4 shadow flex flex-col w-2/5 bg-base-100">
+            <div class="rounded-md p-4 shadow flex flex-col w-2/5 bg-base-100">
                 <h2 class="text-lg font-bold mb-4">My Requests</h2>
 
                 <!-- Make ul fill remaining space and scroll -->
@@ -220,7 +220,7 @@
                     </li>
 
                     <li v-for="request in recentRequests" :key="request.id" @click="showOvertimeRequestModal(request)"
-                        class="card w-full shadow-md border border-base-200 rounded-box p-4 hover:shadow-md hover:border-neutral duration-300 transition-all cursor-pointer">
+                        class="card w-full shadow-md border border-base-200 rounded-box p-4 hover:shadow-md hover:border-primary duration-300 transition-all cursor-pointer">
                         <div class="flex items-center justify-between">
                             <div class="flex flex-col gap-1">
                                 <p class="text-sm opacity-70">{{ request.date }}</p>
@@ -232,7 +232,7 @@
 
                             <div>
                                 <div class="badge badge-outline" :class="{
-                                    'badge-primary': request.status.toUpperCase() === 'PENDING',
+                                    'badge-warning': request.status.toUpperCase() === 'PENDING',
                                     'badge-success': request.status.toUpperCase() === 'APPROVED',
                                     'badge-error': request.status.toUpperCase() === 'DISAPPROVED' || request.status.toUpperCase() === 'CANCELED'
                                 }">
@@ -243,7 +243,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="w-3/5 flex flex-col justify-center p-4 border rounded-md shadow-md bg-base-100">
+            <div class="w-3/5 flex flex-col justify-center p-4 rounded-md shadow-md bg-base-100">
                 <header class="flex items-center justify-between mb-4">
                     <button class="btn btn-sm btn-neutral" @click="handlePreviousMonth()">&lt;</button>
                     <p class="current-date font-bold text-xl">{{ currentMonthYear }}</p>
