8eb6cd4e83c65c8a5f4133f6cd236698a8ee0690
Mark Ciril <markciril.jamon@gmail.com>
2025-08-06 23:11:36 +0800

    chore: remove the query for pending data since it handles in different page

--- app/Http/Controllers/OvertimeRequestController.php
+++ app/Http/Controllers/OvertimeRequestController.php
@@ -182,37 +182,8 @@ class OvertimeRequestController extends Controller
 
         $week = $request->input('week', Carbon::now()->weekOfYear);
         $year = $request->input('year', Carbon::now()->year);
-
-        $overtimelist = [];
-        $overtime_requests = [];
-        $overtime = null;
         $message = '';
         try {
-            $overtime_requests = DB::table('overtime_requests')->join('schedules', 'schedules.id', '=', 'overtime_requests.employee_schedule_id')->join('users', 'users.id', '=', 'schedules.user_id')
-                ->leftJoin('shift_codes', 'shift_codes.id', '=', 'schedules.shift_id')
-                ->select(
-                    'overtime_requests.id as request_id',
-                    'users.id as user_id',
-                    'users.name',
-                    'users.employeeid',
-                    'users.role',
-                    'users.email',
-                    'overtime_requests.start_time',
-                    'overtime_requests.end_time',
-                    'schedules.date',
-                    'schedules.week',
-                    'shift_codes.code as shift_code',
-                    'shift_codes.start_time as shift_start',
-                    'shift_codes.end_time as shift_end',
-                    'overtime_requests.start_time',
-                    'overtime_requests.end_time',
-                    'overtime_requests.hours',
-                    'overtime_requests.status',
-                    'overtime_requests.reason',
-                    'overtime_requests.remarks',
-                    'overtime_requests.created_at'
-                )->where('status', 'PENDING')->whereYear('schedules.date', $year)->where('schedules.week', $week)->orderBy('created_at')->get();
-
             $required_registered_hours = DB::table('required_hours')->where('year', $year)->where('week', $week)->orderBy('updated_at', 'desc')->select('required_hours.required_hours as hours')->first();
             $totals = DB::table('overtime_requests')
                 ->join('schedules', 'schedules.id', '=', 'overtime_requests.employee_schedule_id')
@@ -233,44 +204,6 @@ class OvertimeRequestController extends Controller
             $total_pending = (int)$totals->total_pending;
             $total_hours = $totals->total_hours;
             $required_hours = $required_registered_hours->hours;
-
-            foreach ($overtime_requests as $overtime) {
-                // create instance on timestamps
-                $overtime_start = Carbon::createFromFormat('H:i:s', $overtime->start_time);
-                $overtime_end = Carbon::createFromFormat('H:i:s', $overtime->end_time);
-
-                $schedule_start = $overtime->shift_start === null ? '--' :  Carbon::createFromFormat('H:i:s', $overtime->shift_start);
-                $schedule_end = $overtime->shift_start === null ? '--' :  Carbon::createFromFormat('H:i:s', $overtime->shift_end);
-
-                $overtime_created = Carbon::createFromFormat('Y-m-d H:i:s', $overtime->created_at);
-
-                $overtimelist[] = [
-                    'id' => $overtime->request_id,
-                    'user' => [
-                        'name' => $overtime->name,
-                        'employee_id' => $overtime->employeeid,
-                        'email' => $overtime->email,
-                        'role' => $overtime->role,
-                    ],
-                    'schedule' => [
-                        'date' => $overtime->date,
-                        'week' => $overtime->week,
-                        'shift_code' => $overtime->shift_code ?? 'N/A',
-                        'shift_start' => $schedule_start === '--' ? '--' : $schedule_start->format('h:i A'),
-                        'shift_end' => $schedule_end === '--' ? '--' : $schedule_end->format('h:i A'),
-                    ],
-                    'overtime' => [
-                        'start_time' => $overtime_start->format('h:i A'),
-                        'end_time' =>  $overtime_end->format('h:i A'),
-                        'hours' => $overtime->hours,
-                        'status' => $overtime->status,
-                        'reason' => $overtime->reason,
-                        'remarks' => $overtime->remarks,
-                        'created_at' => $overtime_created->format('l, jS \of F Y, h:i:s A')
-                    ]
-                ];
-            }
-
             $success = true;
         } catch (\Throwable $th) {
             $success = false;
@@ -279,7 +212,6 @@ class OvertimeRequestController extends Controller
 
         return inertia('Approver/Index', [
             'info' => [
-                'requests' => $overtimelist,
                 'totals' => [
                     'required_hours' => $required_hours ?? 0,
                     'total_hours' => $total_hours,
