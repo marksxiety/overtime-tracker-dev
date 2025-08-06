9e190bd26ea499cf2f59a6569ab4ce3095c7d791
Mark Ciril <markciril.jamon@gmail.com>
2025-08-06 19:20:40 +0800

    feat: implement Link if the routename is provided

--- resources/js/Pages/Components/Card.vue
+++ resources/js/Pages/Components/Card.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="stat bg-base-100 shadow">
+    <component :is="routename ? Link : 'div'" :href="routename || undefined" :class="[
+        'stat bg-base-100 shadow',
+        routename ? 'hover:bg-primary hover:cursor-pointer transition-colors duration-200' : ''
+    ]">
         <div class="stat-title">{{ title }}</div>
         <div class="stat-value">{{ value }}</div>
-    </div>
+    </component>
 </template>
 
 <script setup>
+import { Link } from '@inertiajs/vue3'
+
 defineProps({
     title: String | Number,
-    value: String | Number
+    value: String | Number,
+    routename: String
 })
 </script>
