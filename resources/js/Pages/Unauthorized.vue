bfae0c2646cea0562f6cb549f9e88d3aa9f47bc9
Mark Ciril <markciril.jamon@gmail.com>
2025-08-09 14:47:10 +0800

    style: use hero to display the 404 page

--- resources/js/Pages/Unauthorized.vue
+++ resources/js/Pages/Unauthorized.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 class="text-4xl font-bold text-red-600 mb-4">403 - Unauthorized</h1>
-        <p class="text-gray-700 text-lg mb-6">
-            You do not have permission to access this page.
-        </p>
-        <Link href="/" class="text-blue-600 hover:underline">
-        ← Go back to Home
-        </Link>
+    <div class="hero bg-base-300 min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">404 Not Found</h1>
+                <p class="py-6">The page cannot be found.</p>
+                <Link :href="route('main')" class="btn btn-neutral">Return to Home</Link>
+            </div>
+        </div>
     </div>
 </template>
 
