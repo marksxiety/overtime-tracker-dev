f50c3c9f9c60fddf4d432d9560beac4e28a158a6
Mark Ciril <markciril.jamon@gmail.com>
2025-08-09 15:34:55 +0800

    style: improve the design of navbar contents using dropdown and proper hover/gap display

--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -3,35 +3,49 @@
         <div
             class="flex justify-between items-center bottom-2 shadow-md px-4 py-2 mb-6 bg-base-100 rounded h-14 sticky top-0 z-10">
             <Link :href="route('main')" class="font-bold text-sm">Overtime Tracker</Link>
-            <div class="flex justify-between items-center gap-4 font-semibold">
-                <Link v-if="$page?.props?.auth.user?.role === 'approver'" :href="route('hours')" class=" text-sm hover:underline
-                hover:cursor-pointer">Manage ROA</Link>
+            <div class="flex justify-between items-center gap-2 font-semibold">
+                <Link v-if="$page?.props?.auth.user?.role === 'approver'" :href="route('hours')"
+                    class=" text-sm hover:cursor-pointer hover:bg-base-300 py-2 px-4 rounded-3xl">Manage ROA</Link>
                 <Link v-if="$page?.props?.auth.user?.role === 'approver'" :href="route('shifts')"
-                    class="text-sm hover:underline hover:cursor-pointer">Shift Codes</Link>
-                <ul class="menu menu-horizontal px-1" v-if="$page?.props?.auth?.user">
-                    <li class="relative z-10">
-                        <details>
-                            <summary @click="toggleDropdownProfile()">Profile</summary>
-                            <ul class="bg-base-100 rounded-t-none p-2">
-                                <li>
-                                    <Link :href="('logout')" method="post" @click="toggleDropdownProfile()">LogOut
-                                    </Link>
-                                </li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
-                <label class="swap swap-rotate">
-                    <input type="checkbox" class="theme-controller" value="light" />
-                    <svg class="swap-off h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path
-                            d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                    </svg>
-                    <svg class="swap-on h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path
-                            d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                    </svg>
-                </label>
+                    class="text-sm hover:cursor-pointer hover:bg-base-300 py-2 px-4 rounded-3xl">Shift Codes</Link>
+                <div v-if="$page?.props?.auth.user" class="dropdown dropdown-end">
+                    <div tabindex="0" role="button"
+                        class="flex gap-2 text-sm items-center hover:bg-base-300 py-2 px-4 rounded-3xl">
+                        <Icon icon="hugeicons:settings-02" width="24" height="24" />
+                        Settings
+                    </div>
+                    <ul tabindex="0"
+                        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-40 p-2 shadow">
+                        <li>
+                            <a class="justify-between px-4 py-2">
+                                <Icon icon="iconamoon:profile-circle-fill" width="24" height="24" />
+                                Profile
+                            </a>
+                        </li>
+                        <li>
+                            <label class="swap swap-rotate justify-between px-4 py-2 ">
+                                Theme
+                                <input type="checkbox" class="theme-controller" value="light" />
+                                <svg class="swap-off h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24">
+                                    <path
+                                        d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                                </svg>
+                                <svg class="swap-on h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24">
+                                    <path
+                                        d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                                </svg>
+                            </label>
+                        </li>
+                        <li>
+                            <Link :href="('logout')" method="post" class="justify-between px-4 py-2">
+                            <Icon icon="ic:round-logout" width="24" height="24" />
+                            Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
         <main class="pr-20 pl-20 f-full h-screen bg-base-300">
@@ -45,6 +59,7 @@
 import { Link } from '@inertiajs/vue3'
 import Toast from '../Pages/Components/Toast.vue'
 import { ref, provide } from 'vue'
+import { Icon } from "@iconify/vue"
 
 const toastRef = ref()
 
