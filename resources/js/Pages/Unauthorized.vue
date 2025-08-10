2e705d1e67c36683941b715a5a64e75011a52273
Mark Ciril <markciril.jamon@gmail.com>
2025-08-10 08:36:55 +0800

    style: include image for 404

--- resources/js/Pages/Unauthorized.vue
+++ resources/js/Pages/Unauthorized.vue
@@ -2,6 +2,7 @@
     <div class="hero bg-base-300 min-h-screen">
         <div class="hero-content text-center">
             <div class="max-w-md">
+                <img :src="Image404" alt="404">
                 <h1 class="text-5xl font-bold">404 Not Found</h1>
                 <p class="py-6">The page cannot be found.</p>
                 <Link :href="route('main')" class="btn btn-neutral">Return to Home</Link>
@@ -12,4 +13,5 @@
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
+import Image404 from '../images/Match-not-found.svg'
 </script>
