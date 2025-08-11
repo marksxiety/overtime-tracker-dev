57786ef4f8c160418ceb79d29764a9d97f183229
Mark Ciril <markciril.jamon@gmail.com>
2025-08-11 19:06:51 +0800

    style: use minimun width so that when the screen size takes too small, the select content doesnt compressed

--- resources/js/Pages/Components/SelectOption.vue
+++ resources/js/Pages/Components/SelectOption.vue
@@ -2,7 +2,7 @@
     <div :class="[margin, 'w-full']">
         <label v-if="name" for="role" class="block mb-2">{{ name }}</label>
         <select id="role" v-model="model"
-            :class="['w-full px-4 py-2 input', message ? 'border-red-500 focus:ring-red-200' : 'focus:ring-blue-200', 'w-full px-4 py-2 select']">
+            :class="['min-w-24 w-full px-4 py-2 input', message ? 'border-red-500 focus:ring-red-200' : 'focus:ring-blue-200', 'w-full px-4 py-2 select']">
             <option v-for="option in options" :key="option.value" :value="option.value" class="text-center">
                 {{ option.label }}
             </option>
