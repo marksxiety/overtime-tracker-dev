0bb5b22691a957574fb87fcc770c3d5f90d71d20
Mark Ciril <markciril.jamon@gmail.com>
2025-08-14 18:22:21 +0800

    feat: format the holiday 'date' to more readable format

--- resources/js/Pages/api/upcomingHolidays.js
+++ resources/js/Pages/api/upcomingHolidays.js
@@ -5,6 +5,17 @@ let currentYear = new Date().getFullYear();
 let currentDate = new Date().toLocaleDateString("en-CA", {
     timeZone: "Asia/Manila",
 });
+
+function formatHolidayDate(date) {
+    let dateInstance = new Date(date);
+
+    return dateInstance.toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+    });
+}
+
 let url = `https://date.nager.at/api/v3/publicholidays/${currentYear}/PH`;
 
 export default async function fetchUpcomingHolidays() {
@@ -13,11 +24,12 @@ export default async function fetchUpcomingHolidays() {
     let upcoming_holidays = [];
 
     if (Array.isArray(response) && response.length > 0) {
-        upcoming_holidays = response.filter(holiday => holiday.date >= currentDate)
-            .map(holiday => ({
-                date: holiday.date,
+        upcoming_holidays = response
+            .filter((holiday) => holiday.date >= currentDate)
+            .map((holiday) => ({
+                date: formatHolidayDate(holiday.date),
                 localName: holiday.localName,
-                name: holiday.name
+                name: holiday.name,
             }));
     }
 
