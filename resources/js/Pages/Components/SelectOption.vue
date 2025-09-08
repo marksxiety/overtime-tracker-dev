724e6712f99a70ea4e5bb74e10d1cd3218a5a081
Mark Ciril <markciril.jamon@gmail.com>
2025-09-08 22:20:01 +0800

    chore: add props for disabled in select option

--- resources/js/Pages/Components/SelectOption.vue
+++ resources/js/Pages/Components/SelectOption.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="[margin, 'w-full']">
         <label v-if="name" for="role" class="block mb-2">{{ name }}</label>
-        <select id="role" v-model="model"
+        <select id="role" v-model="model" :disabled="disabled"
             :class="['px-4 py-2 input', message ? 'border-red-500 focus:ring-red-200' : 'focus:ring-blue-200', 'w-full px-4 py-2 select', minwidth]">
             <option v-for="option in options" :key="option.value" :value="option.value" class="text-center">
                 {{ option.label }}
@@ -27,6 +27,10 @@ defineProps({
     minwidth: {
         type: String,
         default: 'min-w-24 w-full'
+    },
+    disabled: {
+        type: Boolean,
+        default: false
     }
 })
 
