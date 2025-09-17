98bf6dd71d3212171279839ce77acbfb65ac64e2
Mark Ciril <markciril.jamon@gmail.com>
2025-09-18 01:42:56 +0800

    feat: include the org id in insertion and valdiation to avoid duplication of data

--- app/Http/Controllers/RequiredHoursController.php
+++ app/Http/Controllers/RequiredHoursController.php
@@ -6,19 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\RequiredHours;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class RequiredHoursController extends Controller
 {
     public function registerRequiredHours(Request $request)
     {
 
-        $data = $request->validate([
+        $request->validate([
             'year' => 'required|integer|max_digits:4',
             'week' =>  'required|integer|max_digits:2',
             'required_hours' =>  'required|integer|max_digits:4'
         ]);
 
-        $isRegistered = RequiredHours::where('year', $request->year)->where('week', $request->week)->exists();
+        $isRegistered = RequiredHours::where('year', $request->year)->where('week', $request->week)->where('organization_unit_id', Auth::user()->organization_unit_id)->exists();
 
         if ($isRegistered) {
             return redirect()->back()->withErrors([
@@ -27,7 +28,14 @@ class RequiredHoursController extends Controller
             ]);
         }
 
-        RequiredHours::create($data);
+        $roa = [
+            'year' => $request->year,
+            'week' => $request->week,
+            'required_hours' => $request->required_hours,
+            'organization_unit_id' =>  Auth::user()->organization_unit_id
+        ];
+
+        RequiredHours::create($roa);
         return redirect()->back()->with(['message' => 'Required Hours for week has been registered']);
     }
 
@@ -35,7 +43,7 @@ class RequiredHoursController extends Controller
     {
 
         try {
-            $requiredhours = RequiredHours::query()->orderBy('year', 'desc')->orderBy('week', 'desc')->get();
+            $requiredhours = RequiredHours::query()->where('organization_unit_id', Auth::user()->organization_unit_id)->orderBy('year', 'desc')->orderBy('week', 'desc')->get();
 
             return inertia('Maintenance/RequiredHours', [
                 'requiredhours' => $requiredhours
