9a03eae59bed5a9d076b151042fdab266278e47b
Mark Ciril <markciril.jamon@gmail.com>
2025-09-17 23:52:09 +0800

    chore: update the route for directing the user to reigster form

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['guest'])->group(function () {
-    Route::inertia('/register', 'Auth/Register')->name('register');
+    Route::get('/register', [AuthController::class, 'directRegisterForm'])->name('register');
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::inertia('/login', 'Auth/Login')->name('login');
