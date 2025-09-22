022c091ab6090a67272d85f721f8209a3af6bdf1
Mark Ciril <markciril.jamon@gmail.com>
2025-09-22 10:33:23 +0800

    chore: add the typography in plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,6 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [require("daisyui"), require("@tailwindcss/typography")],
     daisyui: { themes: ["light", "dark"] },
 };
