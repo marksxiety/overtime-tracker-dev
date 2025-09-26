e81b57a71e21d5b41d8d3204397a117c17c47a6b
Mark Ciril <markciril.jamon@gmail.com>
2025-09-26 09:22:06 +0800

    feat: create a list of status in able to populate status dropdown

--- resources/js/Pages/utils/dropdownOptions.js
+++ resources/js/Pages/utils/dropdownOptions.js
@@ -17,29 +17,47 @@ for (let w = 1; w < 52; w++) {
 }
 
 export function currentWeek(date = new Date()) {
-  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  const pastDaysOfYear = (date - firstDayOfYear + (firstDayOfYear.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000) / 86400000;
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear =
+        (date -
+            firstDayOfYear +
+            (firstDayOfYear.getTimezoneOffset() - date.getTimezoneOffset()) *
+                60 *
+                1000) /
+        86400000;
 
-  // adjust if the starting of the week is sunday or monday
-  // just add + 1 if starting on monday
-  const weekNumber = Math.ceil((pastDaysOfYear + firstDayOfYear.getDay()) / 7);
-  return weekNumber;
+    // adjust if the starting of the week is sunday or monday
+    // just add + 1 if starting on monday
+    const weekNumber = Math.ceil(
+        (pastDaysOfYear + firstDayOfYear.getDay()) / 7
+    );
+    return weekNumber;
 }
 
 export function getTimeOptions() {
-    const times = []
-    const pad = n => (n < 10 ? '0' + n : n)
+    const times = [];
+    const pad = (n) => (n < 10 ? "0" + n : n);
 
     for (let h = 0; h < 24; h++) {
         for (let m = 0; m < 60; m += 15) {
-            const hour12 = h % 12 || 12
-            const ampm = h < 12 ? 'AM' : 'PM'
+            const hour12 = h % 12 || 12;
+            const ampm = h < 12 ? "AM" : "PM";
 
             times.push({
                 label: `${pad(hour12)}:${pad(m)} ${ampm}`,
-                value: `${pad(h)}:${pad(m)}`
-            })
+                value: `${pad(h)}:${pad(m)}`,
+            });
         }
     }
-    return times
+    return times;
+}
+
+export const statuses = [];
+const statusList = ["Pending", "In Progress", "Completed", "Cancelled"];
+
+for (const status of statusList) {
+    statuses.push({
+        label: status,
+        value: status.toUpperCase().replace(/ /g, "_"),
+    });
 }
