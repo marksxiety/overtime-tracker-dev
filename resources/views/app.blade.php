c50c2bf1cc8da98e34cab12ab6ff35a78e9abccf
Mark Ciril <markciril.jamon@gmail.com>
2025-10-15 15:08:19 +0800

    fix: add missing CSRF token meta tag in app layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8" />
   <link rel="icon" href="{{ asset('assets/dev.png') }}" type="image/png">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   @vite(['resources/css/app.css', 'resources/js/app.js'])
   @inertiaHead
   @routes
