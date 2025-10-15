45850bf99efa6455caf44eed9864de99d4beb89f
Mark Ciril <markciril.jamon@gmail.com>
2025-10-15 08:13:45 +0800

    revert: remove env based model since it can be change in UI in future improvements

--- resources/js/Pages/services/openai.js
+++ resources/js/Pages/services/openai.js
@@ -5,15 +5,8 @@ const openai = new OpenAI({
     dangerouslyAllowBrowser: true,
 });
 
-const openAImodel = import.meta.env.VITE_OPENAI_API_MODEL ?? "";
 
 export async function analyzeWithAI(jsonData, onChunk) {
-    if (!openAImodel) {
-        return {
-            success: false,
-            data: "OpenAI model is not defined in environment variables.",
-        };
-    }
     try {
         const content =
             typeof jsonData === "string"
@@ -21,7 +14,7 @@ export async function analyzeWithAI(jsonData, onChunk) {
                 : JSON.stringify(jsonData, null, 2);
 
         const result = await openai.chat.completions.create({
-            model: openAImodel,
+            model: 'gpt-4o-mini',
             stream: true,
             messages: [
                 {
@@ -74,16 +67,9 @@ export async function analyzeWithAI(jsonData, onChunk) {
 }
 
 export async function enhanceReasonWithAI(reason) {
-    if (!openAImodel) {
-        return {
-            success: false,
-            data: "OpenAI model is not defined in environment variables.",
-        };
-    }
-
     try {
         const result = await openai.chat.completions.create({
-            model: openAImodel,
+            model: 'gpt-4o-mini',
             messages: [
                 {
                     role: "system",
